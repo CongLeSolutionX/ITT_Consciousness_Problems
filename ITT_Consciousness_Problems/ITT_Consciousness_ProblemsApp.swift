0ccae08a434d31859293dd8d3c8acde0c31745c4
Cong Le <longchike@gmail.com>
2025-06-29 22:49:25 -0700

    The setup

---
+++ ITT_Consciousness_Problems/ITT_Consciousness_ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITT_Consciousness_ProblemsApp.swift
+//  ITT_Consciousness_Problems
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ITT_Consciousness_ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
